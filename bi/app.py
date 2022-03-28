2d078a47834292756d7e4f2dc45c5364ea51dbe1
Ndxxxu-Ndichu <ndichumwangi7@gmail.com>
2022-03-28 11:23:06 +0300

    initial commit

--- bi/app.py
+++ bi/app.py
@@ -18,7 +18,7 @@ app = Blueprint('app', __name__)
 
 @app.route('/')
 def index():
-    posts = Blogpost.query.order_by('id desc')
+    posts = Blogpost.query.order_by(Blogpost.id, desc)
     return render_template('index.html', posts=posts)
 
 @app.route('/', methods=['POST'])
