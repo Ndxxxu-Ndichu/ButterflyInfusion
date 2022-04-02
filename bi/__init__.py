0d7374495e6f279e5c0b785254839477090a27cc
Ndxxxu-Ndichu <ndichumwangi7@gmail.com>
2022-04-02 15:36:29 +0300

    initial commit

--- bi/__init__.py
+++ bi/__init__.py
@@ -14,7 +14,7 @@ def create_app():
      app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:123456@localhost:5432/butterfly_blog"
     else:
         app.config['SECRET_KEY'] = 'bibciejbdxhasxhajabchbc'
-        app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://gppnardpcpltbm:fc4c9dcd6d6f64048d6781d667e530545283a7e59f7fb545cf02a4bb897aaada@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d54a03ddvlhch6H"
+        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://gppnardpcpltbm:fc4c9dcd6d6f64048d6781d667e530545283a7e59f7fb545cf02a4bb897aaada@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d54a03ddvlhch6H"
 
 
     db.init_app(app)
