d3857c5e1c4ac7a95712752d92bf74ee2b78aaa1
Ndxxxu-Ndichu <ndichumwangi7@gmail.com>
2022-04-13 13:19:06 +0300

    initial commit

--- bi/models.py
+++ bi/models.py
@@ -2,7 +2,7 @@ from . import db
 
 class Blogpost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    blog_id = db.Column(db.Integer)
+    blog_id = db.Column(db.Integer, unique=True)
     author = db.Column(db.String(50))
     date = db.Column(db.String(15))
     title = db.Column(db.String(100))
