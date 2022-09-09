2dbcd1d50fe98b069151565fe6442ab78e8de46f
Ndichu Mwangi <ndichumwangi7@gmail.com>
2022-09-09 11:39:19 +0300

    initial commit

--- bi/s3.py
+++ bi/s3.py
@@ -5,8 +5,8 @@ import boto3, botocore
 def upload_file(file_name, bucket):
     object_name = file_name
     s3_client = boto3.client('s3',
-                             aws_access_key_id='AKIAYGIXF7RWTHF5TFKN',
-                             aws_secret_access_key ='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',
+                             aws_access_key_id='',
+                             aws_secret_access_key ='',
                              )
     response = s3_client.upload_file(file_name, bucket, object_name)
     return response
